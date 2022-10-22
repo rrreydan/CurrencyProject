375678392495a42354890ac5911a7e2703fa8414
Andrey Deykun <rrreydan@gmail.com>
2022-10-22 12:43:29 +0300

    Added the ability to select all currencies at once. Currencies are now displayed as a table

--- currencies/views.py
+++ currencies/views.py
@@ -6,15 +6,30 @@ from django.shortcuts import render
 
 def show_currency(request):
     currencies = Currency.objects.all()
-    currency_id = int(request.GET.get('id', default=1))
 
-    info_about_currency = {}
+    if request.GET.get('is_all'):
+        list_of_currencies = []
+        data_dict = service.get_dict_of_currencies()
 
-    data_dict = service.get_dict_of_currencies()
+        for i in currencies:
+            info_about_currency = {}
 
-    for i in data_dict.keys():
-        for j in data_dict[i].keys():
-            if j == currency_id - 1:
-                info_about_currency[i] = data_dict[i][currency_id - 1]
+            for j in data_dict.keys():
+                for k in data_dict[j].keys():
+                    if k == i.id - 1:
+                        info_about_currency[j] = data_dict[j][i.id - 1]
 
-    return render(request, 'show_currency.html', {'info': info_about_currency, 'currencies': currencies})
+            list_of_currencies.append(info_about_currency)
+
+        return render(request, 'show_currency.html', {'info_all': list_of_currencies, 'currencies': currencies})
+    else:
+        currency_id = int(request.GET.get('id', default=1))
+        info_about_currency = {}
+        data_dict = service.get_dict_of_currencies()
+
+        for i in data_dict.keys():
+            for j in data_dict[i].keys():
+                if j == currency_id - 1:
+                    info_about_currency[i] = data_dict[i][currency_id - 1]
+
+        return render(request, 'show_currency.html', {'info': info_about_currency, 'currencies': currencies})
