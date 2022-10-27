86460c08c6d494c5f14602d68f5ba2a9eabe3b25
Andrey Deykun <rrreydan@gmail.com>
2022-10-27 18:47:34 +0300

    Significantly simplified the code in views.py , added a few comments

--- currencies/views.py
+++ currencies/views.py
@@ -7,17 +7,17 @@ from django.shortcuts import render
 def show_currency(request):
     currencies = Currency.objects.all()
 
+    # Check whether the item with the withdrawal of all currencies is selected
+    # if yes, then create a list and fill it with all currencies
     if request.GET.get('is_all'):
         list_of_currencies = []
         data_dict = service.get_dict_of_currencies()
 
-        for i in currencies:
+        for currency_id in range(len(currencies)):
             info_about_currency = {}
 
-            for j in data_dict.keys():
-                for k in data_dict[j].keys():
-                    if k == i.id - 1:
-                        info_about_currency[j] = data_dict[j][i.id - 1]
+            for i in data_dict:
+                info_about_currency.update({i: data_dict.get(i).get(currency_id)})
 
             list_of_currencies.append(info_about_currency)
 
@@ -27,9 +27,7 @@ def show_currency(request):
         info_about_currency = {}
         data_dict = service.get_dict_of_currencies()
 
-        for i in data_dict.keys():
-            for j in data_dict[i].keys():
-                if j == currency_id - 1:
-                    info_about_currency[i] = data_dict[i][currency_id - 1]
+        for i in data_dict:
+            info_about_currency.update({i : data_dict.get(i).get(currency_id - 1)})
 
         return render(request, 'show_currency.html', {'info': info_about_currency, 'currencies': currencies})
