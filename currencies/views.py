e33c17452cbb8ec5e59b651faded0e80990b888e
Andrey Deykun <rrreydan@gmail.com>
2022-10-31 20:06:51 +0300

    Site design change, many minor improvements (mainly related to the date)

--- currencies/views.py
+++ currencies/views.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from . import service
 from .models import Currency
 
@@ -11,10 +13,14 @@ def show_currency(request):
     # if yes, then create a list and fill it with all currencies
     if request.GET.get('is_all'):
         return render(request, 'show_currency.html', {'info': service.get_currencies(request, True),
-                                                      'currencies': currencies})
+                                                      'currencies': currencies,
+                                                      'chosen_date': request.GET.get('date'),
+                                                      'date_now': str(datetime.now())[:10]})
     else:
         return render(request, 'show_currency.html', {'info': service.get_currencies(request),
-                                                      'currencies': currencies})
+                                                      'currencies': currencies,
+                                                      'chosen_date': request.GET.get('date'),
+                                                      'date_now': str(datetime.now())[:10]})
 
 
 def export_currency(request):
